d5c80d4d85abfe1078b0b102afe33f03f0641289
Filipe Oliveira <hi@fmoliveira.dev>
2022-08-14 09:10:01 -0400

    Deploy contract

--- week2/hardhat.config.ts
+++ week2/hardhat.config.ts
@@ -1,8 +1,19 @@
 import { HardhatUserConfig } from "hardhat/config"
 import "@nomicfoundation/hardhat-toolbox"
 
+require("dotenv").config()
+
+const GOERLI_URL: string = process.env.GOERLI_URL!
+const PRIVATE_KEY: string = process.env.PRIVATE_KEY!
+
 const config: HardhatUserConfig = {
 	solidity: "0.8.9",
+	networks: {
+		goerli: {
+			url: GOERLI_URL,
+			accounts: [PRIVATE_KEY],
+		},
+	},
 }
 
 export default config
